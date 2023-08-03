61564c1720bc0fca9e993cf8df10e4c63f8655ed
sirenuf <viktor6153@hotmail.com>
2023-08-04 01:51:21 +0200

    Add new drawing for board, and handling.

--- game.c
+++ game.c
@@ -1,59 +1,108 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 
-const int X_AMOUNT = 8;
-const int Y_AMOUNT = 8;
+#define X_AMOUNT 8
+#define Y_AMOUNT 8
 
-void drawMapToScreen()
+struct Cell
 {
-    /*
-    * Draws the map to the screen.
-    */
+    bool isMine;
+    bool isRevealed;
+    int  neighbourMines;
+};
 
-    system("clear");
+struct Cell board[X_AMOUNT][Y_AMOUNT];
 
-    /* Y loop keeps redrawing X field with a new line. */
-    for (int y = 0; y < Y_AMOUNT; y++) {
-        /* Draw the actual characters on x loop. */
-        printf("%d|", y);
-        for (int x = 0; x < X_AMOUNT; x++) {
-            if (x+1 == X_AMOUNT)
-                printf("X");
-            else
-                printf("X ");
+void init_board(void)
+{
+    for (int x = 0; x < X_AMOUNT; x++) {
+        for (int y = 0; y < Y_AMOUNT; y++) {
+            struct Cell *target = &board[x][y];
+
+            target->isMine = false;
+            target->isRevealed = false;
+            //target->neighbourMines = 0; To be made
         }
-
-        printf("|\n");
     }
+}
 
-    // Prints wall between before X cords.
-    printf("  ");
+void draw_board(void)
+{
+    printf(" ┌");
     for (int x = 0; x < X_AMOUNT; x++) {
         if (x+1 == X_AMOUNT)
-            printf("-");
+            printf("-┐\n");
         else
             printf("--");
     }
 
-    // Prints X cords
+    for (int y = 0; y < Y_AMOUNT; y++) {
+        printf(" |");
+        for (int x = 0; x < X_AMOUNT; x++) {
+            struct Cell *target = &board[x][y];
+
+            if (target->isMine) {
+                printf("It's a mine!\n"); // Soon make the game exit.
+                return;
+            } else if (target->isRevealed) {
+                target->isRevealed = true;
+                printf("-");
+            } else {
+                printf("X");
+            }
+
+            if (x+1 != X_AMOUNT)
+                printf(" ");
+            else
+                printf("|%d", y+1);
+
+        }
+        printf("\n");
+    }
+
+    printf(" └");
+    for (int x = 0; x < X_AMOUNT; x++) {
+        if (x+1 == X_AMOUNT)
+            printf("-┘");
+        else
+            printf("--");
+    }
+    
     printf("\n  ");
     for (int x = 0; x < X_AMOUNT; x++)
-        printf("%d ", x);
+        printf("%d ", x+1);
 
-    printf("\n\n");
 }
+
+
+void reveal_cell(int X, int Y)
+{
+    struct Cell *target = &board[X][Y];
+    printf("X: %d\nY: %d", X, Y);
+    target->isRevealed = true;
+}
+
 
 int main(void)
 {
-    int Running = true;
+    // Make cell struct in main and pass
+    // to draw_board.
+    int PlaceX;
+    int PlaceY;
+    bool Running = true;
 
-    while(Running)
-    {
-        drawMapToScreen();
-        scanf("%d", &Running);
+    init_board();
+
+    while (Running) {
+        draw_board();
+
+        printf("\n>");
+        scanf("%d %d", &PlaceX, &PlaceY);
+        PlaceX--;
+        PlaceY--;
+        reveal_cell(PlaceX, PlaceY);
+        printf("\n");
     }
 
-    printf("\n");
     return 0;
 }
